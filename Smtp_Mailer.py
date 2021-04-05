55369d76e37926eeb5838c56b8ba7a5cf4857c05
hichamdz38 <dachirhicham@gmail.com>
2021-04-05 23:11:41 +0100

    fix main script

--- Smtp_Mailer.py
+++ Smtp_Mailer.py
@@ -1,9 +1,6 @@
 #!usr/bin/python 
 # -*- coding: utf-8 -*-
 import threading,sys, smtplib, socket, time
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
- 
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
@@ -14,6 +11,8 @@ from config import*
 import random
 import re
 
+
+# load accounts credential from smtpf file
 accounts=[]
 try: 
 	L=open(smtpf)
@@ -25,6 +24,7 @@ try:
 except: 
 	print("\n[+] We were unable to open the smtp file. Check again your path and try again.")
 
+# load jobs from jobsf file
 try: 
 	L=open(jobsf)
 	jobs = L.read().split('\n')
@@ -32,14 +32,12 @@ try:
 except: 
 	print("\n[+] We were unable to open the jobs file. Check again your path and try again.")
 
+# load body from bodyf file
 try: 
-	L=open(bodyf,encoding="utf8")
-	subject1,body1 = L.read().split("\n#\n")
-	#print('read  file')
-	#print(subject1,body1)
-	#print('close file')
-	subject1=subject1
-	body1=body1
+	L=open(bodyf, encoding="utf8")  # unicode support
+	subject1, body1 = L.read().split("\n#\n")
+	subject1 = subject1
+	body1 = body1
 	L.close()
 except Exception as e: 
 	print("\n[+] We were unable to open the file. Check again your path and try again.",bodyf,e)
@@ -48,23 +46,31 @@ print("limit number of thread is : ",thread_limit)
 
 
 def timer():
-        now = time.localtime(time.time())
-        return time.asctime(now)
+	""" get the actual time"""
+    now = time.localtime(time.time())
+    return time.asctime(now)
 
 def get_smtp(index=random.randint(0,len(accounts)-1)):
+	"""return a random account from the accounts list"""
 	return accounts[index]
 
+
 def get_proxy(index):
+	""" return a random proxy from the proxies list"""
 	return proxies[index%len(proxies)]
 
 
-def sendchk(body,subject,job):   # seperated function for checking
+def sendchk(body, subject, job):   # seperated function for checking
+	""" this is the main function that send email
+	it's need three arguments:
+	body, subject, job"""
 	global N,jobs
 	Err_N=0
 	while True:
 		try:
+			# get credential from file
 			host,user,password,prt = get_smtp(N).split(':')
-			if prt=="465":
+			if prt == "465":
 	                    smtp = smtplib.SMTP_SSL(host,int(prt))
 			else:
 	                    smtp = smtplib.SMTP(host,int(prt))
@@ -74,18 +80,15 @@ def sendchk(body,subject,job):   # seperated function for checking
 				code = smtp.helo()[0]
 				if not (200 <= code <= 299):
 					raise SMTPHeloError(code, resp)
-			fromaddr = job[1]
-			toaddr = job[3]
-			reply=job[4]
+			fromaddr = job[1]  # sender
+			toaddr = job[3]  # receiver
+			reply = job[4]  # repley to
 			msg= MIMEText(body, 'plain', 'utf-8')
-			msg['Subject'] = subject
-			msg['To'] = formataddr([job[2],job[3]])
-			msg['From'] =formataddr([job[0],job[1]])
-			msg['Reply-to'] =formataddr([job[0],job[4]])
-			#print("----- messgae -----")
-			#print(msg)
-			#print("----- messgae -----")
-			smtp.sendmail(fromaddr, toaddr, msg.as_string())
+			msg['Subject'] = subject  # the email's subject
+			msg['To'] = formataddr([job[2], job[3]])  # receiver name, receiver email 
+			msg['From'] = formataddr([job[0], job[1]])  # sender name, sender email
+			msg['Reply-to'] = formataddr([job[0], job[4]])  # reply to name, reply to email
+			smtp.sendmail(fromaddr, toaddr, msg.as_string())  # send the email
 			print("\n\t\t[!] Email Sent Successfully:",host, user, password)
 			smtp.quit()
 			Err_N=0
@@ -100,8 +103,9 @@ def sendchk(body,subject,job):   # seperated function for checking
 				return
 			print("Try with other smtp")
 
-N=0
-L_th=[]
+
+N = 0  # hold the account index
+L_th = []  # hold the active threads
 print("[!] Start at: " + timer() + "")
 for j in jobs:
 	if j!="":
@@ -115,9 +119,6 @@ for j in jobs:
 			subject = re.sub('_To', job[2], subject)
 			subject = re.sub('_Femail', job[1], subject)
 			subject = re.sub('_Temail', job[3], subject)
-			#print(body)
-			#print(subject)
-			#print(get_smtp(N))
 			print(get_smtp(N).split(":"))
 			print("\n\t[+] start thread",":".join(job))
 			th=Thread(target=sendchk,args=(body,subject,job,))
@@ -131,13 +132,15 @@ for j in jobs:
 			print(e)
 		N=(N+1)%len(accounts)
 
+
+# wait for all threads to done
 for i in L_th:
 	i.join()
 print("[!] Ended at: " + timer() + "")
 print("reformat the smtp file")
-N=(N-1)%len(accounts)
-accounts=accounts[N+1:]+accounts[:N+1]
 
+N = (N-1)%len(accounts)
+accounts=accounts[N+1:]+accounts[:N+1]
 L=open(smtpf,"w")
 for i in accounts:
 	L.write(i+"\n")
