c78ed243d4d5e5455304a42db0b99fbb0fd0a0e4
hichamdz38 <dachirhicham@gmail.com>
2021-04-05 22:54:53 +0100

    config file

---
+++ config.py
@@ -0,0 +1,5 @@
+thread_limit=4  	# the threading limit
+proxy=0           	# 0: desactivate proxy, 1: activate proxy
+smtpf ="smtp2.txt"	# smtp accounts file
+jobsf  ="jobs.txt"		# job file
+bodyf ="body.txt"		# body file
